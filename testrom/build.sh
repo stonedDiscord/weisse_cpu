144db8afbe8c97569143ab2695cf35fa4680b8be
stonedDiscord <Tukz@gmx.de>
2025-12-12 12:21:14 +0000

    use z80 target

--- testrom/build.sh
+++ testrom/build.sh
@@ -1,2 +1,2 @@
 # docker run  -v .:/src/ -it z88dk/z88dk z88dk-sccz80 main.c
-docker run  -v .:/src/ -it z88dk/z88dk zcc +rc2014 -subtype=uart85 -v --c-code-in-asm --list main.c -o testrom -create-app
+docker run  -v .:/src/ z88dk/z88dk zcc +z80 -clib=8085 -pragma-define:CRT_ORG_BSS=0xc0000 main.c -create-app
