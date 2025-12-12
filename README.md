2d650272e7520662f036dda9714f1b5dd82f04c6
stonedDiscord <Tukz@gmx.de>
2025-12-12 10:17:47 +0000

    testrom

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Dies ist ein Reverse-Engineering Projekt fuer die "Elektronische Steuereinheit" von adp.
 
-Hierbei geht es mehr um die Platine selbst als den Schaltplan, da sich die Platine von meinem Disc 2000 langsam auflöst.
+Die Platine ist 4087-000101 mit 3 EPROM-Sockeln aus meinem Disc 2000.
+
+IO Ports:
+0x50 - 0x51 8279
+0x60 - 0x6f 8256
+0x70 - 0x71 Walzen etc
+0x72        Sound
+
+Im Gegensatz zu den 4040 Platinen wo diese noch etwas anders verteilt sind.
+Das ist auch der Grund wieso die ROMs inkompatibel sind.
---
+++ testrom/main.c
@@ -0,0 +1,89 @@
+/* IO Ports:
+0x50 - 0x51 8279
+0x60 - 0x6f 8256
+0x72        Sound
+*/
+
+#include <stdint.h>
+
+#define KDC_DATA 0x50
+#define KDC_CMD 0x51
+
+#define WRITE_DISPLAY 0x80
+
+void io_out(uint8_t port, uint8_t data) {
+    /*
+    __asm
+    out (port), data
+    __endasm;
+    */
+}
+
+uint8_t io_in(uint8_t port) {
+    /*
+    __asm
+    in (port)
+    __endasm;
+    */
+}
+
+void kdc_cmd_out(uint8_t data) {
+    io_out(KDC_CMD, data);
+}
+
+void kdc_data_out(uint8_t data) {
+    io_out(KDC_DATA, data);
+}
+
+uint8_t kdc_cmd_in() {
+    return io_in(KDC_CMD);
+}
+
+uint8_t kdc_data_in() {
+    return io_in(KDC_DATA);
+}
+
+void writeLamps(uint8_t line, uint8_t data) {
+    kdc_cmd_out(WRITE_DISPLAY | line);
+    kdc_data_out(data);
+}
+
+void writeDigits(uint8_t digit, uint8_t mon, uint8_t srv) {
+    kdc_cmd_out(WRITE_DISPLAY | digit + 8);
+    kdc_data_out((mon << 4) | srv);
+}
+
+void init_kdc() {
+    kdc_cmd_out(0x0C);
+    kdc_cmd_out(0x3E);
+    kdc_cmd_out(0xC1);
+    kdc_cmd_out(0xE0);
+}
+
+void main(void) {
+    uint8_t i;
+
+    init_kdc();
+
+    // lamps
+    for (i = 0; i < 8; i++) {
+        writeLamps(i,0xFF);
+    }
+
+    // 7segment displays
+    for (i = 0; i < 8; i++) {
+        writeDigits(i,0x03,0x04);
+    }
+
+    uint8_t last_key = 0;
+
+    // Infinite loop to keep lamps on and scan keyboard
+    while (1) {
+        uint8_t status = kdc_cmd_in();
+        if ((status & 0x04) == 0) { // FIFO not empty
+            last_key = kdc_data_in();
+        }
+        writeDigits(0, last_key >> 4, 0);
+        writeDigits(1, 0, last_key & 0x0F);
+    }
+}
