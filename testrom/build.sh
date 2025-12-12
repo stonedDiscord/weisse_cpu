1119c49eaf42df253549c4d446cc6de804b177b2
stonedDiscord <Tukz@gmx.de>
2025-12-12 10:40:27 +0000

    switch to z88dk

--- testrom/build.sh
+++ testrom/build.sh
@@ -1,1 +1,2 @@
-sdcc -mmcs51 main.c
+# docker run  -v .:/src/ -it z88dk/z88dk z88dk-sccz80 main.c
+docker run  -v .:/src/ -it z88dk/z88dk zcc +rc2014 -subtype=acia85 -v --c-code-in-asm --list main.c -o testrom -create-app
