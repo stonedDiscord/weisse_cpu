d507855bd158b0bc51314efa2b3bf29d74348265
stonedDiscord <Tukz@gmx.de>
2025-12-13 14:22:01 +0100

    caveman asm

--- testrom/build.sh
+++
@@ -1,1 +0,0 @@
-docker run  -v .:/src/ z88dk/z88dk zcc +z80 -clib=8085 -pragma-define:CRT_ORG_BSS=0xc0000 main.c -create-app
---
+++ testrom/main.asm
@@ -0,0 +1,99 @@
+; 8085 Assembly program for GNUSim8085
+; Target: 8279 Keyboard/Display Controller
+
+; IO Ports:
+; 0x50 - 0x51 8279
+; 0x60 - 0x6f 8256
+; 0x72        Sound
+
+jmp     start
+
+;data
+KDC_DATA:      EQU 50H
+KDC_CMD:       EQU 51H
+WRITE_DISPLAY: EQU 80H
+
+; Initialize KDC
+KDCINI: MVI     A, 0CH
+        OUT     KDC_CMD
+        MVI     A, 3EH
+        OUT     KDC_CMD
+        MVI     A, 0C1H
+        OUT     KDC_CMD
+        MVI     A, 0E0H
+        OUT     KDC_CMD
+        RET
+
+; Write to lamps
+; Input: C = line number, B = data
+WRLAMP: MOV     A, C
+        ORI     WRITE_DISPLAY
+        OUT     KDC_CMD
+        MOV     A, B
+        OUT     KDC_DATA
+        RET
+
+; Write to digits
+; Input: C = digit number, B = mon, D = srv
+WR_DIG: MOV     A, C
+        ADI     8
+        ORI     WRITE_DISPLAY
+        OUT     KDC_CMD
+        MOV     A, B
+        RLC
+        RLC
+        RLC
+        RLC
+        ORA     D
+        OUT     KDC_DATA
+        RET
+
+; Main program
+start:  CALL    KDCINI
+        ; Turn on all lamps
+        MVI     B, 0FFH
+        MVI     C, 0
+LAMPLP: CALL    WRLAMP
+        INR     C
+        MOV     A, C
+        CPI     8
+        JNZ     LAMPLP
+        ; Initialize all digits
+        MVI     B, 03H
+        MVI     D, 04H
+        MVI     C, 0
+DIGILP: CALL    WR_DIG
+        INR     C
+        MOV     A, C
+        CPI     8
+        JNZ     DIGILP
+        ; Infinite loop to scan keyboard
+MAINLP: IN      KDC_CMD
+        ANI     04H
+        JNZ     NO_KEY
+        ; Key pressed - read it
+        IN      KDC_DATA
+        MOV     B, A
+        JMP     DISPKY
+NO_KEY: MOV     B, 0
+DISPKY: MOV     A, B
+        RRC
+        RRC
+        RRC
+        RRC
+        ANI     0FH
+        MOV     D, A
+        MVI     C, 0
+        MVI     B, 0
+        CALL    WR_DIG
+
+        MOV     A, B
+        ANI     0FH
+        MOV     D, A
+        MVI     C, 1
+        MVI     B, 0
+        CALL    WR_DIG
+
+        JMP     MAINLP
+
+        HLT
