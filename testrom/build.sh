e7d0478c8cbb49ee6b7b3dcefc3b569f3a76c22d
stonedDiscord <Tukz@gmx.de>
2025-12-14 14:34:27 +0100

    pad and roms folder

--- testrom/build.sh
+++ testrom/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 docker run  -v .:/src/ z88dk/z88dk z88dk-z80asm -m8085 -b gs_main.asm
+
+# Pad gs_main.bin to 256 bytes
+current_size=$(stat -c%s gs_main.bin 2>/dev/null || echo 0)
+if [ $current_size -lt 256 ]; then
+    dd if=/dev/zero bs=1 count=$((256 - current_size)) >> gs_main.bin 2>/dev/null
+fi
+
 rm testrom.zip
 7z a testrom.zip gs_main.bin
+cp testrom.zip /run/media/stoned/schrott/Roms/mame/roms/
