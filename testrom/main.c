9fc037ef2473fe563c9704dcfae498d3c5167f44
stonedDiscord <Tukz@gmx.de>
2025-12-21 23:55:02 +0100

    sound

--- testrom/main.c
+++ testrom/main.c
@@ -86,6 +86,56 @@ uint8_t kdc_data_in() {
 }
 
 /**
+ * @brief Play a sound note
+ *
+ * @param note Note value to play
+ */
+void playSound(uint8_t note) {
+    uint8_t test = note;
+	__asm
+        OUT 0x72
+    __endasm;
+}
+
+typedef enum {
+    NOTE_CL = 0,
+    NOTE_C = 1,
+    NOTE_B = 2,
+    NOTE_AS = 3,
+    NOTE_A = 4,
+    NOTE_GS = 5,
+    NOTE_G = 6,
+    NOTE_FS = 7,
+    NOTE_F = 8,
+    NOTE_E = 9,
+    NOTE_DS = 10,
+    NOTE_D = 11,
+    NOTE_CS = 12,
+} note_t;
+
+typedef enum {
+    A8 = 0,
+    A7 = 1,
+    A6 = 2,
+    A5 = 3,
+} octave_t;
+
+typedef enum {
+    DURATION_EIGHTH = 0,
+    DURATION_QUARTER = 1,
+    DURATION_HALF = 2,
+    DURATION_WHOLE = 3,    
+} duration_t;
+
+struct noteData
+{
+    /* data */
+    note_t note: 4;
+    uint8_t octave: 2;
+    duration_t duration: 2;
+};
+
+/**
  * @brief Read data from the 8256 MUART's Port 1
  *
  * @return uint8_t Data read from port
