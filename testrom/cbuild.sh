cf6d1ddbaec4b79f8cef745084349ecfc22468b9
stonedDiscord <Tukz@gmx.de>
2026-02-21 22:45:24 +0100

    build old rom

--- testrom/cbuild.sh
+++ testrom/cbuild.sh
@@ -10,3 +10,17 @@ rm fullhous.zip
 
 cp testromc.zip /run/media/stoned/schrott/Roms/mame/roms/
 cp fullhous.zip /run/media/stoned/schrott/Roms/mame/roms/
+
+docker run  -v .:/src/ z88dk/z88dk sh ./build.sh
+
+split -b 2048 a.rom excellent_part_
+mv excellent_part_aa excellent.ice5
+mv excellent_part_ab excellent.ice6
+mv excellent_part_ac excellent.icd5
+mv excellent_part_ad excellent.icd6
+mv excellent_part_ae excellent.icc5
+rm excellnt.zip
+7z a excellnt.zip excellent.ice5 excellent.ice6 excellent.icd5 excellent.icd6 excellent.icc5
+
+
+cp excellnt.zip /run/media/stoned/schrott/Roms/mame/roms/excellnt.zip
